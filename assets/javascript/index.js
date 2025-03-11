49151e4cdcedbb116915a15d72d4d396caa1069b
Noah <Noah580@users.noreply.github.com>
2025-03-11 15:57:29 +0100

    Repart de zéro car manque d'inspiration

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("c'est chargé")
-})
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('Document bien chargée')
+});
